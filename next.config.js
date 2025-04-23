73184258f2dab5b73e49c25c1267307bad9998dc
Az-soft <Az-soft>
2025-04-24 02:01:45 +0500

    Update Next.js configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/az-soft' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/az-soft/' : '',
+  basePath: '/az-soft',
+  assetPrefix: '/az-soft/',
   images: {
     unoptimized: true
   },
